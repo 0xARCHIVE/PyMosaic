0cd0ac25b1ff6eba43581944b11d546a6a2a1a8d
Steven <texruska@users.noreply.github.com>
2016-01-06 17:21:26 +0000

    Update dota_icon_wallpaper.py

--- dota_icon_wallpaper.py
+++ dota_icon_wallpaper.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jan  5 18:33:42 2016
-
-@author: Steven
-"""
-
 from lib import fetch_icons
 from lib import process_icons
 
 subdir_name = 'Dota2_icons'
-#fetch_icons.fetch_icons(subdir_name)
+#fetch_icons.fetch_icons(subdir_name) #fetch_icons.fetch_icons(subdir_name) downloads a bunch of Dota 2 icons and sticks them into './subdir_name/'
 #process_icons.dota_process_icons(subdir_name)
 
-process_icons.image_replicate(subdir_name)
+process_icons.image_replicate(subdir_name)  #the actual mosaic generating function. It duplicates "./collage_target.png", so make sure to create one!
 
 print('Complete')
